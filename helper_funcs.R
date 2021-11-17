eab6118ea77aae8b8951a503e78a320d4a5e69cd
shanaa <s.modcha@gmail.com>
2021-11-16 23:25:18 -0500

    bayes anovas added

--- helper_funcs.R
+++ helper_funcs.R
@@ -4,20 +4,27 @@ library(effsize)     # for Cohen's d
 library(BayesFactor) # to compute Bayes factors
 library(tidyverse)
 
-plot_group_density <- function(df, group, y, title = "Distributions within Groups"){
+plot_group_density <- function(df, group, y, subgroup = NULL, title = "Distributions within Groups"){
   # plot some density things for visualizing data in groups
   # group, and y are strings
-  
-  p <- df %>% 
-    ggplot(aes(.data[[group]],.data[[y]])) +
-    geom_violin(aes(fill=.data[[group]]), alpha=0.2, draw_quantiles = c(.25, .5, .75), scale = "count") +
-    geom_beeswarm() +
-    stat_summary(fun=mean, geom="point", size=3, color="red") +
-    ggtitle(title)
+  if (is.null(subgroup)){
+    p <- df %>% 
+      ggplot(aes(.data[[group]],.data[[y]])) +
+      geom_violin(aes(fill=.data[[group]]), alpha=0.2, draw_quantiles = c(.25, .5, .75), scale = "count") +
+      geom_beeswarm(alpha = 0.5) +
+      stat_summary(fun=mean, geom="point", size=3, color="red") +
+      ggtitle(title)
+  }
+  else{
+    p <- df %>% 
+      ggplot(aes(.data[[group]],.data[[y]], colour = .data[[subgroup]])) +
+      geom_beeswarm(dodge.width = .9, alpha = 0.3) +
+      geom_violin(aes(fill=.data[[subgroup]]), alpha=0.2, draw_quantiles = c(.25, .5, .75), scale = "count") +
+      ggtitle(title)
+  }
   
   return(p)
 }
-
 
 bayes_t_test <- function(df, group_title, group1, group2, dv){
   # bayes t-test
@@ -66,4 +73,124 @@ bayes_t_test <- function(df, group_title, group1, group2, dv){
   alt <- 1 / (1 + res.bayes$BF01)
   
   return (sprintf("BF01: %.3f, P(D|H0): %.3f, P(D|H1): %.3f, Groups: %s vs %s", res.bayes$BF01, null, alt, group1, group2))
+}
+
+#####
+# No Cursors
+
+apply_nocur_blcorrection <- function(rot_df_row, bl_df){
+  # no cursor baseline correction
+  # make sure input is in format: targetangle_deg, ppt, angular_dev
+  
+  bl <- filter(bl_df, 
+               targetangle_deg == rot_df_row[1] & 
+                 ppt == rot_df_row[2])$angular_dev %>% 
+    mean(na.rm = TRUE)
+  
+  
+  corrected_dev <- as.numeric(rot_df_row[3]) - bl
+  
+  return(corrected_dev)
+}
+
+##### 
+# Learning curves
+
+apply_training_blcorrection <- function(rot_df_row, bl_df){
+  # training baseline correction
+  # make sure input is in format: targetangle_deg, ppt, angular_dev
+  
+  bl <- filter(bl_df, 
+               targetangle_deg == rot_df_row[1] & 
+                 ppt == rot_df_row[2])$angular_dev %>% 
+    mean(na.rm = TRUE)
+  
+  
+  corrected_dev <- as.numeric(rot_df_row[3]) - bl
+  
+  return(corrected_dev)
+}
+
+add_block_num <- function(df){
+  # given a df with experiment and block number
+  # these if statements are repetitive at the moment. I've kept them in case different trials need to be isolated for different experiment protocols
+  x <- as.double(df[1])
+  if(df[2] == "gradualExp"){
+    if (x <= 69)
+      return(0)
+    else if (x >= 127 & x <= 132)
+      return(1)
+    else if (x >= 193 & x <= 198)
+      return(2)
+    else if (x >= 259 & x <= 264)
+      return(3)
+    else if (x >= 325 & x <= 330)
+      return(4)
+    else
+      return(10)
+  }
+  else if(df[2] == "stepwiseExp") {
+    if (x <= 69)
+      return(0)
+    else if (x >= 127 & x <= 132)
+      return(1)
+    else if (x >= 193 & x <= 198)
+      return(2)
+    else if (x >= 259 & x <= 264)
+      return(3)
+    else if (x >= 325 & x <= 330)
+      return(4)
+    else
+      return(10)
+  }
+  else { #for longAbrupt
+    if (x <= 69)
+      return(0)
+    else if (x >= 127 & x <= 132)
+      return(1)
+    else if (x >= 193 & x <= 198)
+      return(2)
+    else if (x >= 259 & x <= 264)
+      return(3)
+    else if (x >= 325 & x <= 330)
+      return(4)
+    else
+      return(10)
+  }
+}
+
+add_trial_set <- function(df){
+  # given a df with experiment and trial sets
+  # these if statements are repetitive at the moment. I've kept them in case different trials need to be isolated for different experiment protocols
+  x <- as.double(df[1])
+  if(df[2] == "gradualExp"){
+    if (x <= 69)
+      return(1) # first block
+    else if (x >= 127 & x <= 129)
+      return(2) # the end of training
+    else if (x >= 322)
+      return(3) # the final 9 trials
+    else
+      return(10)
+  }
+  else if(df[2] == "stepwiseExp") {
+    if (x <= 69)
+      return(1)
+    else if (x >= 307 & x <= 309)
+      return(2)
+    else if (x >= 322)
+      return(3)
+    else
+      return(10)
+  }
+  else { #for longAbrupt
+    if (x <= 69)
+      return(1)
+    else if (x >= 109 & x <= 111)
+      return(2)
+    else if (x >= 322)
+      return(3)
+    else
+      return(10)
+  }
 }
