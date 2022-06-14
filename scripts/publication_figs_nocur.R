eef187c731ebf6e2385e90f610a2bb4efb2cef32
Shanaa Modchalingam <s.modcha@gmail.com>
2022-06-14 12:25:26 -0400

    decay figure changes

--- scripts/publication_figs_nocur.R
+++ scripts/publication_figs_nocur.R
@@ -472,6 +472,26 @@ make_decay_figure <- function() {
     data_group <- data_group %>%
         mutate(y_inferred = group_slope_mean * trial_in_block +
             group_intercept_mean)
+    
+    #### TESTING ####
+    data_group2 <- data %>%
+      group_by(exp, strat_use, block_num) %>%
+      summarise(
+        ind_mean = mean(angular_dev),
+        sd = sd(angular_dev),
+        ci = vector_confint(angular_dev),
+        slope = decay_fit(trial_in_block, angular_dev)[2],
+        intercept = decay_fit(trial_in_block, angular_dev)[1],
+        confint_low = reg_confints(trial_in_block, angular_dev)[[1]][1:9],
+        confint_up = reg_confints(trial_in_block, angular_dev)[[2]][1:9],
+        n = n(), .groups = "drop"
+      )
+    # add trial in block
+    data_group2$trial_in_block = rep(c(1:9), length.out = nrow(data_group2))
+    # make a column with inferred y values
+    data_group2 <- data_group2 %>%
+      mutate(y_inferred = slope * trial_in_block +
+               intercept)
 
 
     # plot the figure using data_group,
@@ -500,19 +520,34 @@ make_decay_figure <- function() {
 
     # add data
     p <- p +
+        # geom_line(
+        #     data = data_group,
+        #     aes(y = y_inferred),
+        #     size = 1,
+        #     alpha = 0.5,
+        #     colour = "red"
+        # ) +
         geom_line(
-            data = data_group,
-            aes(y = y_inferred),
-            size = 1,
-            alpha = 1
+          data = data_group2,
+          aes(y = y_inferred),
+          size = 1,
+          alpha = 1
         ) +
-        geom_smooth(
-            method = "lm",
-            se = TRUE,
-            level = 0.99,
-            size = 0.5,
-            alpha = 0.1
+        geom_ribbon(
+          data = data_group2,
+          aes(y = y_inferred,
+              ymin = confint_low,
+              ymax = confint_up),
+          size = 1,
+          alpha = 0.2,colour = NA
         ) +
+        # geom_smooth(
+        #     method = "lm",
+        #     se = TRUE,
+        #     level = 0.99,
+        #     size = 0.5,
+        #     alpha = 0.1
+        # ) +
         facet_grid(
             strat_use ~ block_num,
             space = "free"
@@ -520,7 +555,7 @@ make_decay_figure <- function() {
 
     # set y axis from 0 to 45
     p <- p + scale_y_continuous(
-        limits = c(0, 35),
+        limits = c(-10, 40),
         breaks = c(0, 15, 30),
         labels = c(0, 15, 30)
     ) + scale_x_continuous(
@@ -551,6 +586,144 @@ make_decay_figure <- function() {
     return(p)
 }
 
+make_decay_implicit_figure <- function() {
+  # load in the DECAY data
+  data <- load_bl_corrected("data/bl_corrected/bl_corrected_data.csv",
+                            type = "nocursor"
+  )
+  
+  # add a column with 1-9 repeating
+  data <- data %>%
+    mutate(trial_in_block = rep(1:9, length.out = nrow(data)))
+  
+  # select useful columns
+  data <- data %>%
+    select(exp, ppt, strat_use, block_num, trial_in_block, angular_dev) %>%
+    group_by(exp, ppt, strat_use, block_num, trial_in_block) %>%
+    summarise(
+      angular_dev_m = mean(angular_dev),
+      n = n()
+    ) %>%
+    pivot_wider(
+      names_from = strat_use,
+      names_prefix = "strat_",
+      values_from = angular_dev_m
+    ) %>%
+    mutate(strategy = strat_1 - strat_0) %>%
+    pivot_longer(
+      cols = 6:8,
+      names_to = "strat_use", values_to = "angular_dev"
+    )
+  
+  # in strat_use, replace "strat_0" with "Without Strategy"
+  # and "strat_1" with "With Strategy",
+  # and "strategy" with "Explicit Strategy"
+  data <- data %>%
+    mutate(strat_use = ifelse(strat_use == "strat_0", "Without Strategy",
+                              ifelse(strat_use == "strat_1", "With Strategy",
+                                     ifelse(strat_use == "strategy", "Explicit Strategy",
+                                            strat_use
+                                     )
+                              )
+    ))
+  
+  # make strat_use a factor
+  data <- data %>%
+    mutate(strat_use = factor(strat_use,
+                              levels = c("Without Strategy", "With Strategy", "Explicit Strategy")
+    )) %>%
+    filter(strat_use == "Without Strategy") %>%
+    unite(exp_block, exp, block_num)
+  
+  # make individual table
+  data_ind <- data %>%
+    group_by(exp_block, ppt, strat_use) %>%
+    summarise(
+      ind_mean = mean(angular_dev),
+      sd = sd(angular_dev),
+      ci = vector_confint(angular_dev),
+      slope = decay_fit(trial_in_block, angular_dev)[2],
+      intercept = decay_fit(trial_in_block, angular_dev)[1],
+      n = n(), .groups = "drop"
+    )
+  
+  # make group table
+  data_group <- data_ind %>%
+    group_by(exp_block, strat_use) %>%
+    summarise(
+      group_slope_mean = mean(slope),
+      slope_ci = vector_confint(slope),
+      group_intercept_mean = mean(intercept),
+      intercept_ci = vector_confint(intercept),
+      n = n(), .groups = "drop"
+    )
+  
+  # color is the experiment type
+  p <- data_group %>%
+    ggplot(aes(
+      x = exp_block, y = group_slope_mean,
+      colour = exp_block, fill = exp_block
+    ))
+  
+  # add data points
+  p <- p +
+    geom_beeswarm(
+      data = data_ind,
+      aes( y = slope),
+      alpha = 0.1,
+      size = 1
+    ) +
+    geom_linerange(aes(ymin = group_slope_mean - slope_ci, 
+                       ymax = group_slope_mean + slope_ci),
+                   alpha = 0.5, lwd = 2
+    ) +
+    geom_point(
+               size = 2, alpha = 1
+    )
+  
+  # add theme changes
+  p <- p + theme_classic() +
+    xlab("Block") + ylab("Decay Rate (°/trial)") +
+    geom_hline(
+      yintercept = 0,
+      linetype = "solid",
+      size = 0.4,
+      colour = "#CCCCCC"
+    )
+  
+  # set colour palette
+  p <- p + scale_color_manual(values = c(
+    pallete$abrupt, pallete$abrupt, pallete$abrupt, pallete$abrupt, 
+    pallete$ramped,pallete$ramped,pallete$ramped,pallete$ramped,
+    pallete$stepped,pallete$stepped,pallete$stepped,pallete$stepped
+  ), labels = c(
+    "Abrupt","Abrupt","Abrupt","Abrupt",
+    "Ramp", "Ramped","Ramped","Ramped",
+    "Step", "Step", "Step", "Step"
+  )) 
+  
+  # change x axis text labels
+  p <- p + scale_x_discrete(
+    labels = c(
+      "1", "2", "3", "4",
+      "1", "2", "3", "4",
+      "1", "2", "3", "4"
+    )
+  )
+  
+  # # set y axis ticks and labels
+  # p <- p + scale_y_continuous(
+  #   limits = c(-10, 40),
+  #   breaks = c(0, 15, 30),
+  #   labels = c(0, 15, 30)
+  # )
+  # 
+  # remove the legend
+  #p <- p + theme(legend.position = "none")
+  
+  return(p)
+}
+
 # Note: all final figures will have a width of 7 inches
 # save the implicit figure
 ggsave(make_implicit_figure(),
@@ -575,3 +748,12 @@ ggsave(make_decay_figure(),
     height = 6, width = 7, device = "pdf",
     filename = "data/paper_figs/decay_aes.pdf"
 )
+
+
+
+
+test <- data %>% 
+  filter(ppt == "001_longAbruptExp", block_num == 1,
+         strat_use == "Without Strategy")
+
+reg_confints(test$trial_in_block, test$angular_dev)
--- src/helper_funcs.R
+++ src/helper_funcs.R
@@ -39,6 +39,50 @@ decay_fit <- function(x_vec, y_vec) {
   return(coefs)
 }
 
+
+reg_confints <- function(x, y) {
+  n <- length(y) # Find length of y to use as sample size
+  lm.model <- lm(y ~ x) # Fit linear model
+  
+  # Extract fitted coefficients from model object
+  b0 <- lm.model$coefficients[1]
+  b1 <- lm.model$coefficients[2]
+  
+  # Find SSE and MSE
+  sse <- sum((y - lm.model$fitted.values)^2)
+  mse <- sse / (n - 2)
+  
+  t.val <- qt(0.995, n - 2) # Calculate critical t-value
+  
+  # Fit linear model with extracted coefficients
+  x_new <- 1:max(x)
+  y.fit <- b1 * x_new + b0
+  
+  # Find the standard error of the regression line
+  se <- sqrt(sum((y - y.fit)^2) / (n - 2)) * sqrt(1 / n + (x - mean(x))^2 / sum((x - mean(x))^2))
+  
+  # Fit a new linear model that extends past the given data points (for plotting)
+  # x_new2 <- 1:max(x + 100)
+  # y.fit2 <- b1 * x_new2 + b0
+  
+  # Warnings of mismatched lengths are suppressed
+  slope.upper <- suppressWarnings(y.fit + t.val * se)
+  slope.lower <- suppressWarnings(y.fit - t.val * se)
+  
+  # Collect the computed confidence bands into a data.frame and name the colums
+  bands <- data.frame(cbind(slope.lower, slope.upper))
+  colnames(bands) <- c('Lower Confidence Band', 'Upper Confidence Band')
+  
+  # Plot the fitted linear regression line and the computed confidence bands
+  # plot(x, y, cex = 1.75, pch = 21, bg = 'gray')
+  # lines(y.fit, col = 'black', lwd = 2)
+  # lines(bands[1], col = 'blue', lty = 2, lwd = 2)
+  # lines(bands[2], col = 'blue', lty = 2, lwd = 2)
+  
+  return(bands)
+}
+
+
 # get the magnitude (euclidian normal) of a vector (this is faster than R's built in norm)
 norm_vec <- function(vector) {
   sqrt(crossprod(vector))
